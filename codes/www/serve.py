4b91204c5cd851fc202b44ac922c628cb71a09a0
Aaaarbon <aaronorosenfb@gmail.com>
2022-11-08 13:21:46 -0800

    updating codes to makew

--- codes/www/serve.py
+++ codes/www/serve.py
@@ -1,10 +1,36 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+import mimetypes
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 
 
-httpd = HTTPServer(('0.0.0.0', 4443), SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               keyfile="privkey.pem",
-                               certfile='fullchain.pem', server_side=True)
+def get_file(request):
+    # note that this potentially makes every file on your computer readable by the internet
+    print(request.path)
+    directory = "/home/ar/MSARHP/codes/www"
+    # try keep requested limited to directory
+    path = request.path.split("?")[0]
+    if path == '/':
+        path = '/index.html'
 
+
+    f = open(directory + path, "rb")
+    request.send_response(200)
+    request.send_header(
+                'Content-type',  mimetypes.guess_type(directory + path)[0])
+
+    request.end_headers()
+    request.wfile.write(f.read())
+    f.close()
+
+    return request.wfile.write('debug response condition')
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        return get_file(self)
+
+
+httpd = HTTPServer(("0.0.0.0", 1279), SimpleHTTPRequestHandler)
 httpd.serve_forever()
+
