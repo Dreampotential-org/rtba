2f5424a90424ee4c73e895272b42e899f8fb92e7
Aaron Rosen <aaronorosen@gmail.com>
2023-01-16 09:58:44 -0500

    add account login api integration

--- codes/www/js/profile_ia.js
+++ codes/www/js/profile_ia.js
@@ -153,64 +153,16 @@ function text_to_speech(seconds, speedFlow) {
 }
 
 function configure_events() {
-    $("#start").on("click", function(ev) {
+    $("#new_account_create").on("click", function(ev) {
         ev.preventDefault();
         signup_api({
-            name: 'StarterName',
-            email: $("#account").val(),
-            password: $("#password").val(),
-            days_sober: null,
+            name: $('#account_name').val(),
+            email: $("#new_account_email").val(),
+            password: $("#new_account_password").val(),
         });
     })
 }
 
-
-
-function signup_api(params) {
-    if (params.days_sober == null) {
-        params.days_sober = "0";
-    }
-    var form = new FormData();
-    form.append("name", params.name);
-    form.append("email", params.email);
-    form.append("days_sober", params.days_sober);
-    form.append("sober_date", null);
-    form.append("password", params.password);
-    form.append("source", window.location.host);
-
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
-    form.append("page", page.toLowerCase());
-
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: SERVER + "api/create-user/",
-        method: "POST",
-        processData: false,
-        contentType: false,
-        mimeType: "multipart/form-data",
-        data: form,
-    };
-    $.ajax(settings).done(function (response) {
-        if (Object.keys(JSON.parse(response)).includes('token')) {
-            localStorage.setItem("session_id",
-                JSON.parse(response).token);
-            swal({
-                title: "Good job!",
-                text: "You're logged in",
-                icon: "success",
-            });
-        }
-    }).fail(function (err) {
-        console.log(err);
-        swal({
-            title: "warning",
-            text: "Invalid email or password",
-            icon: "warning",
-        });
-    });
-}
 
 function screenlock() {
     /*
