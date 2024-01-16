4eaf7d4b64154611255431e835e108fefed9ed03
Ben viP <bengg2040@gmail.com>
2024-01-15 21:11:43 -0800

    start adding this code we are requiring some more backe updates

--- codes/www/js/subscription.js
+++ codes/www/js/subscription.js
@@ -1,36 +1,25 @@
 function init_subscription() {
-    alert("DFDF")
-    let session_id = localStorage.getItem('session_id')
 
     console.log("Sanity check!");
     var subscription_data = null;
     var stripe = null;
-    if (session_id) {
-        settings = get_settings_checkout("config/", "GET");
+    settings = get_settings_checkout("config/", "GET");
+    $.ajax(settings).done(function(response) {
+        data = JSON.parse(response);
+        stripe = Stripe(data.publicKey);
+
+        settings = get_settings_checkout(
+            "retrieve-device-subscription/" + get_finger_print(), "GET")
         $.ajax(settings).done(function(response) {
-            data = JSON.parse(response);
-            stripe = Stripe(data.publicKey);
-
-            settings = get_settings_checkout("retrieve-subscription/", "GET")
-            $.ajax(settings).done(function(response) {
-                subscription_data = JSON.parse(response);
-                console.log("HEre is my subscription status:" + subscription_data)
-            }).then((res) => {
-                return res
-            });
-
+            subscription_data = JSON.parse(response);
+            console.log("HEre is my subscription status:" + subscription_data)
+        }).then((res) => {
+            return res
         });
-    }
-    $(".agentstat-login").click(function() {
-        // if person is not active in their accout ask them to signup
-        if (!(session_id)) {
-            return window.location.href = '/signup/'
-        }
 
-        // if they they have subscription var populated
-        if (subscription_data && subscription_data.subscription === 'true') {
-            return window.location.href = '/Ai/'
-        }
+    });
+    $(".agentstat-login").click(function() {
+    // if person is not active in their accout ask them to signup
         else if (subscription_data) {
             settings = get_settings_checkout("create-checkout-session/", "GET")
             $.ajax(settings).done(function(response) {
